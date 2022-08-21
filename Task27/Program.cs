299cf422510bc0aace1ec73749be603a6cf57b60
AlinaKov <alkovkova@gmail.com>
2022-08-21 14:31:39 +0500

    Добавила отрицательные числа

--- Task27/Program.cs
+++ Task27/Program.cs
@@ -6,9 +6,15 @@
 Console.WriteLine("Введите число");
 string number = Console.ReadLine();
 int N = number.Length;
+int number1 = Convert.ToInt32(number);
 int sum = 0;
+if (number1 < 0)
+{
+    number1 = -1 * number1;
+    N = N - 1;
+}
 for (int i = 0; i < N; i++)
 {
-    sum = sum + number.ToString()[i] - 48; //не могу понять, почему программа увеличивает каждое число на 48. Поэтому теперь я его вычитаю и работает правильно. Но откуда оно взялось - не  знаю, всяко пробовала и проверяла.
+    sum = sum + number1.ToString()[i] - 48; //не могу понять, почему программа увеличивает каждое число на 48. Поэтому теперь я его вычитаю и работает правильно. Но откуда оно взялось - не  знаю, всяко пробовала и проверяла.
 }
 Console.WriteLine(sum);
